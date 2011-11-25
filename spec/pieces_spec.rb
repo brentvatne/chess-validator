c02a54d38c9b0ac2a0d183bdffb553e7f296980c
Brent Vatne <brentvatne@gmail.com>
2011-11-25 17:59:49 -0500

    fix var name that was bothering me

--- spec/pieces_spec.rb
+++ spec/pieces_spec.rb
@@ -3,18 +3,18 @@ describe Chess::Pieces do
   describe Chess::Pieces::Pawn do
     let(:white_pawn) { Chess::Pieces::Pawn.new(:white) }
     let(:black_pawn) { Chess::Pieces::Pawn.new(:black) }
-    let(:pos) { Chess::Coordinates.new(4, 4) }
+    let(:position) { Chess::Coordinates.new(4, 4) }
     let(:white_start) { Chess::Coordinates.new(1, 1) }
     let(:black_start) { Chess::Coordinates.new(6, 1) }
     let(:enemy) { Chess::Pieces::Pawn.new(:black) }
 
     describe "moves" do
       it "should be able to move one row up if white" do
-        white_pawn.moves(pos).should include(Chess::Move.new(1, 0))
+        white_pawn.moves(position).should include(Chess::Move.new(1, 0))
       end
 
       it "should be able to move one row down if black" do
-        black_pawn.moves(pos).should include(Chess::Move.new(-1, 0))
+        black_pawn.moves(position).should include(Chess::Move.new(-1, 0))
       end
 
       describe "first move" do
@@ -29,7 +29,7 @@ describe Chess::Pieces do
 
       describe "enemy in diagonal" do
         it "should be able to move one row up and one row left" do
-          white_pawn.moves(pos, enemy).should include(Chess::Move.new(1, 1))
+          white_pawn.moves(position, enemy).should include(Chess::Move.new(1, 1))
         end
       end
     end
