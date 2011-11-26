f06767223f2938d83bb7931c7f03a7a84a915a84
Brent Vatne <brentvatne@gmail.com>
2011-11-26 14:49:23 -0500

    simplify path_for with inject

--- lib/chess/rules.rb
+++ lib/chess/rules.rb
@@ -19,7 +19,7 @@ module Chess
     end
 
     def occupied?(cell, board = @board)
-      a = board.at(cell) != :empty
+      board.at(cell) != :empty
     end
 
     def open_path_to_destination(board = @board, origin = @origin, destination = @destination)
@@ -27,31 +27,23 @@ module Chess
     end
 
     def path_for(board, origin, destination)
-      paths = []
       delta_row = destination.row - origin.row
       delta_col = destination.column - origin.column
-      return paths if (delta_row <= 1 and delta_col <= 1)
+      return [] if (delta_row <= 1 and delta_col <= 1)
 
       if delta_row.abs == delta_col.abs
-        #diagonal
-        (origin.row+1..destination.row-1).each do |n|
-          paths << Coordinates.new(n, n) unless n == destination.row
+        (origin.row+1..destination.row-1).inject([]) do |paths, n|
+          paths << Coordinates.new(n, n)
         end
       elsif delta_col == 0
-        #vertical
-        (origin.row+1..destination.row-1).each do |n|
+        (origin.row+1..destination.row-1).inject([]) do |paths, n|
           paths << Coordinates.new(origin.row, n)
         end
       elsif delta_row == 0
-        #horizontal
-        (origin.column+1..destination.column-1).each do |n|
-          paths << Coordinates.new(n, origin.column) unless n == destination.column
+        (origin.column+1..destination.column-1).inject([]) do |paths, n|
+          paths << Coordinates.new(n, origin.column)
         end
       end
-
-      p paths
-
-      paths
     end
   end
 end
