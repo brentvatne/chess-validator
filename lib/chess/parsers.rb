05752d096546c4278b908ad9e29c17c4dc7b9cd5
Brent Vatne <brentvatne@gmail.com>
2011-11-27 11:05:51 -0500

    last bit of documentation

--- lib/chess/parsers.rb
+++ lib/chess/parsers.rb
@@ -1,9 +1,20 @@
 module Chess
   module Parsers
 
+    # Public: Parses a chess board into a format that can be used by Board.
     BoardParser = Object.new
 
     class << BoardParser
+      # Public: Parses a board data string into Pieces and Coordinates
+      #
+      # board    - A String, usually will be read out of a file. Open it up
+      #            first with File.open("board_file.txt").read and pass it in.
+      # notation - Optional: A Class that can translate the notation system
+      #            used for the given board. Imeplements the same
+      #            interface as AlgebraicNotation.
+      #
+      # Returns an array of Hashes with keys :piece and :coordinates,
+      # containing a Piece instance and Coordinates instance respectively.
       def parse(board, notation = Notations::AlgebraicNotation)
         pieces = []
 
@@ -18,10 +29,16 @@ module Chess
         pieces
       end
 
+      # Internal: Checks for an empty cells
+      #
+      # Returns true if it has a piece, false if it's empty
       def has_piece?(cell)
         cell != "--"
       end
 
+      # Internal: Iterates over each cell in the board.
+      #
+      # Yields the cell String, column number, and row number.
       def each_cell(board)
         board.split("\n").each_with_index do |row, row_number|
           row.split.each_with_index do |cell, column_number|
@@ -31,15 +48,32 @@ module Chess
       end
     end
 
+    # Public: Parses a list of moves
     MoveParser = Object.new
 
     class << MoveParser
+
+      # Public: Parses a moves data string
+      #
+      # move     - A String, usually will be read out of a file. Open it up
+      #            first with File.open("moves_file.txt").read and pass it in.
+      # notation - Optional: A Class that can translate the notation system
+      #            used for the given board. Imeplements the same
+      #            interface as AlgebraicNotation.
+      #
+      # Returns a Hash with the keys :origin and :destination, whose values
+      # are both Strings in # algebraic notation (default) representing a
+      # position on the board.
       def parse(moves, notation = Notations::AlgebraicNotation)
         reduce_moves(moves) do |parsed, origin, destination|
           parsed << { :origin => origin, :destination => destination }
         end
       end
 
+      # Internal: Keep parse readable by abstracting out the messy inject
+      #
+      # Yields the total Array being accumulated, and the origin and destination
+      # Strings for the given row.
       def reduce_moves(moves)
         moves.split("\n").inject([]) do |total, pair|
           yield(total, *pair.split)
