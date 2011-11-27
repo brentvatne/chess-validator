947ef8b47945d482e98e925d869c3aa16d5af236
Brent Vatne <brentvatne@gmail.com>
2011-11-26 23:30:42 -0500

    refactor board a bit

--- lib/chess/board.rb
+++ lib/chess/board.rb
@@ -1,5 +1,60 @@
 module Chess
+  module BoardAccessorMethods
+    def rows; @board; end
+
+    def columns; @board.transpose; end
+
+    # piece_at?
+    def at(row, column = :blank)
+      if column == :blank
+        position = row
+        position = notation.translate_position(position) if position.is_a? String
+        row    = position.row
+        column = position.column
+      end
+
+      @board[row][column]
+    end
+
+    def each_cell
+      columns.each_with_index do |column, column_number|
+        column.each_with_index do |cell, row_number|
+          yield(cell, column_number, row_number)
+        end
+      end
+    end
+
+    # Internal: Finds the location of a King, given its color
+    #
+    # color - A symbol, either :black or :white, representing the color of the
+    #         King to find.
+    #
+    # Returns a Coordinates instance with the location of the King.
+    def king_position(color)
+      each_cell do |cell, column_number, row_number|
+        if cell.is_a? Pieces::King and cell.color == color
+          return Coordinates.new(column_number, row_number)
+        end
+      end
+    end
+
+    # Internal: Selects every piece on the board
+    #
+    # color - Optional, filters the yielded Pieces to those of the given color.
+    #         Can be either :black or :white. Default is :all
+    #
+    # Yields an instance of a Chess::Pieces::Piece sublcass
+    def positions_of_pieces(color = :all)
+      each_cell do |cell, column_number, row_number|
+        if cell != :empty and (cell.color == color or color == :all)
+          yield Coordinates.new(column_number, row_number)
+        end
+      end
+    end
+  end
+
   class Board
+    include BoardAccessorMethods
     # Gets the notation class
     attr_reader :notation
 
@@ -8,10 +63,6 @@ module Chess
       create_new_board
       load_state(initial_state)
     end
-
-    def rows; @board; end
-
-    def columns; @board.transpose; end
 
     def create_new_board
       @board = Array.new(8).map { |col| col = Array.new(8).fill(:empty) }
@@ -26,24 +77,6 @@ module Chess
       @board[coords.row][coords.column] = piece
     end
 
-    # piece_at?
-    def at(row, column=:blank)
-      if (in_notation = row).kind_of? String
-        coords = translate_to_coordinates(in_notation)
-        row    = coords.row
-        column = coords.column
-      elsif (coords = row).kind_of? Coordinates
-        row    = coords.row
-        column = coords.column
-      end
-
-      @board[row][column]
-    end
-
-    def translate_to_coordinates(alg_position)
-      @notation.translate_position(alg_position)
-    end
-
     # Public: Moves a given piece to another place on the board, ignoring
     # any validation of rules.
     #
@@ -54,44 +87,8 @@ module Chess
     def move_piece!(params)
       from, to = params.values
       add_piece at(from), to
-      empty_cell! from
+      @board[from.row][from.column] = :empty
+      # empty_cell! from
     end
-
-    def empty_cell!(cell)
-      @board[cell.row][cell.column] = :empty
-    end
-
-    # Internal: Finds the location of a King, given its color
-    #
-    # color - A symbol, either :black or :white, representing the color of the
-    #         King to find.
-    #
-    # Returns a Coordinates instance with the location of the King.
-    def king_position(color)
-      columns.each_with_index do |column, column_number|
-        column.each_with_index do |cell, row_number|
-          if cell.kind_of? Pieces::King and cell.color == color
-            return Coordinates.new(column_number, row_number)
-          end
-        end
-      end
-    end
-
-    # Internal: Selects every piece on the board
-    #
-    # color - Optional, filters the yielded Pieces to those of the given color.
-    #         Can be either :black or :white. Default is :all
-    #
-    # Yields an instance of a Chess::Pieces::Piece sublcass
-    def positions_of_pieces(color = :all)
-      columns.each_with_index do |column, column_number|
-        column.each_with_index do |cell, row_number|
-          if cell != :empty and (cell.color == color or color == :all)
-            yield Coordinates.new(column_number, row_number)
-          end
-        end
-      end
-    end
-
   end
 end
